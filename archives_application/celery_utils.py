9c8b2438ebcd2cc7ab140a76e3be799498c16496
aa-dank <aarondankert@gmail.com>
2023-03-24 09:51:24 -0700

    Try adding ContextTask()

--- archives_application/celery_utils.py
+++ archives_application/celery_utils.py
@@ -1,8 +1,15 @@
 from celery import current_app as current_celery_app
+from celery import Task
 
 
 def make_celery(app):
+    class ContextTask(Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return Task.__call__(self, *args, **kwargs)
+
     celery = current_celery_app
     celery.config_from_object(app.config, namespace="CELERY")
-
+    celery.set_default()
+    celery.Task = ContextTask
     return celery
