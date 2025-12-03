b52d74485196ad2474c9f00825a1d530b906c735
aa-dank <aarondankert@gmail.com>
2025-12-03 14:58:37 -0800

    updated worker.py to work with rq 2.6

--- worker.py
+++ worker.py
@@ -2,19 +2,16 @@
 import os
 import redis
 from rq import Worker, Queue
-from rq.connections import RedisConnection
 
 listen = ['default']
-
-redis_url = 'redis://localhost:6379'
-
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
 
 # For Windows we need to use this:
 # https://github.com/michaelbrooks/rq-win
 if __name__ == '__main__':
-    print("Starting worker")
-    with RedisConnection(conn):
-        worker = Worker(list(map(Queue, listen)))
-        print("Worker initialized. Starting work.")
-        worker.work()
+    print("starting worker")
+    queues = [ Queue(name, connection=conn) for name in listen ]
+    worker = Worker(queues, connection=conn)
+    print("worker initialized. starting work.")
+    worker.work() 
